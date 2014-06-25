16a7a59aa20518ec7de9154cd51fa7ea41bab953
Allen Yingyuan Zhang <zyy2013@bu.edu>
2014-06-25 18:32:34 -0400

    Setting CPU and Process Layout

--- js/eventControler.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready( function() {               
-	$("#header").stickUp();		//Header stick
-});
-$('#myModal').modal();
-$('#myModa2').modal();
-$('#myModa3').modal();
---
+++ js/eventController.js
@@ -0,0 +1,94 @@
+
+/*
+ * Click function for modal1
+ */ 
+$('button[data-target="#myModal1"]').click(function(){$('#myModal1').modal({'backdrop': 'static'});});
+
+
+/*
+ * Click function for modal2
+ */ 
+$('button[data-target="#myModal2"]').click(function(){
+	$('#myModal2').modal({'backdrop': 'static'});
+
+	var scheme = $('#scheme').val();
+	var html = "";
+	// Display corresponding setting algorithm page base on the scheme and quantity of cpu used set
+	if (scheme == "global") {
+		html = '<div class="form-group">'
+	            	+'<label class="col-sm-4 control-label">Select Algorithm</label>'
+	                +'<div class="col-xs-5">'
+	                	+'<select class="form-control" id="algorithm">'
+						  +'<option value="default">---- Select a Algorithm ----</option>'
+						  +'<option value="G-EDF">G-EDF</option>'
+						  +'<option value="LLF">LLF</option>'
+						+'</select>'
+	                +'</div>'
+	            +'</div>';
+
+	}
+	else if (scheme == "partitioned") {
+		var cpuQty = $('#cpu-quantity').val();
+		for (var i = 0; i < cpuQty; i++) {
+			html += '<div class="form-group">'
+	                	+'<label class="col-sm-5 control-label">Select Algorithm for CPU' + i + '</label>'
+	                    +'<div class="col-xs-5">'
+	                    	+'<select class="form-control" id="cpu' + i + '-alg">'
+							  +'<option value="default">---- Select a Algorithm ----</option>'
+							  +'<option value="G-EDF">G-EDF</option>'
+							  +'<option value="LLF">LLF</option>'
+							+'</select>'
+	                    +'</div>'
+	                +'</div>';
+        }
+	}
+	$('#algorithm-list').html(html);
+
+});
+
+
+/*
+ * Click function for modal3
+ */ 
+$('button[data-target="#myModal3"]').click(function(){$('#myModal3').modal({'backdrop': 'static'});});
+
+
+/*
+ * Display process setting form base on the quantity of process used set
+ */
+$("#process-quantity").change(function(){
+    $("#process-table").remove();
+	var rowCount= $("#process-quantity").val();
+    var table=$("<table id='process-table' border='0'></table>");
+   	table.appendTo($("#process-table-div"));
+
+	var tr=$("<tr></tr>");
+	tr.appendTo(table);
+	var th=$("<th>PID</th>");
+	th.appendTo(tr);
+	var th=$("<th>Arrive Time</th>");
+	th.appendTo(tr);
+	var th=$("<th>Execution Time</th>");
+	th.appendTo(tr);
+	var th=$("<th>Period</th>");
+	th.appendTo(tr);
+
+	for(var i=0;i<rowCount;i++)
+	{
+		var tr=$("<tr></tr>");
+		tr.appendTo(table);
+		for(var j=0;j<4;j++)
+	{
+		var td=$("<td></td>");
+		td.appendTo(tr);
+		if(j==0){
+		  var text=$("<span>Process"+i+"</span>");
+		  text.appendTo(td);
+		}
+		else{
+			var input=$("<input></input>")
+			input.appendTo(td);
+		}
+	}
+	}
+});

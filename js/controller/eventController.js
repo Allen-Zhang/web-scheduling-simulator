1fc6f9ff7f2032ae228c73f405fa668748820a75
Allen Yingyuan Zhang <zyy2013@bu.edu>
2014-06-28 01:42:35 -0400

    Add referenced algorithm link

--- js/controller/eventController.js
+++ js/controller/eventController.js
@@ -10,9 +10,9 @@ $('#add-case').click(function(){
 /*
  * Click function for Edit Case button
  */ 
-$('#edit-case').click(function(){
-	$('#myModal1').modal({'backdrop': 'static'});
-	editCase();
+$('#edit-process').click(function(){
+	$('#myModal4').modal({'backdrop': 'static'});
+	editProcess();
 });
 
 /*
@@ -64,7 +64,6 @@ $('#show-modal3').click(function(){
 	$('#myModal3').modal({'backdrop': 'static'});
 });
 
-
 /*
  * Display process setting form base on the quantity of process used set
  */
@@ -90,21 +89,20 @@ $("#process-quantity").change(function(){
 		var tr=$("<tr></tr>");
 		tr.appendTo(table);
 		for(var j=0;j<4;j++)
-	{
-		var td=$("<td></td>");
-		td.appendTo(tr);
-		if(j==0){
-		  var text=$("<span>Process"+i+"</span>");
-		  text.appendTo(td);
+		{
+			var td=$("<td></td>");
+			td.appendTo(tr);
+			if(j==0){
+			  var text=$("<span>Process"+i+"</span>");
+			  text.appendTo(td);
+			}
+			else{
+				var input=$("<input id='input"+(i*4+j)+"'class='form-control'></input>");
+				input.appendTo(td);
+			}
 		}
-		else{
-			var input=$("<input id='input"+(i*4+j)+"'class='form-control'></input>");
-			input.appendTo(td);
-		}
-	}
 	}
 });
-
 
 /*
  * Click function for Finish button on modal3
@@ -113,8 +111,8 @@ $('#save-case').click(function(){
 	saveCase();
 	showCaseSettings();
 	showEditCaseButton();
+	showCaseInfo();
 });
-
 
 function saveCase() {
 	var scheme = $('#scheme').val();  // Get the scheme user selected
@@ -167,20 +165,63 @@ function saveCase() {
 	// }
 }
 
+function editProcess() {
+	// Get all process information from saved simulator object respectively
+	var process = simulator.processList;
+	var rowCount = process.length; 
 
-function editCase() {
-	// Get all values from saved simulator object respectively
-	var scheme = simulator.scheme; 
-	var cpuQty = simulator.resourceList.length; 
-	var processQty = simulator.processList.length; 
+	var table=$("<table id='edit-process-table' border='0'></table>");
+   	table.appendTo($("#edit-process-table-div"));
+
+	var tr=$("<tr></tr>");
+	tr.appendTo(table);
+	var th=$("<th>PID</th>");
+	th.appendTo(tr);
+	var th=$("<th>Arrive Time</th>");
+	th.appendTo(tr);
+	var th=$("<th>Execution Time</th>");
+	th.appendTo(tr);
+	var th=$("<th>Period</th>");
+	th.appendTo(tr);
+	var th=$("<th>Delete</th>");
+	th.appendTo(tr);
+
+	for(var i = 0; i < rowCount; i++) {
+		var tr=$("<tr></tr>");
+		tr.appendTo(table);
+		for(var j = 0; j < 5; j++) {
+			var td=$("<td></td>");
+			td.appendTo(tr);
+			if(j==0){
+			  var text=$("<span>Process"+i+"</span>");
+			  text.appendTo(td);
+			}
+			if(j==1){
+				var input=$("<input id='edit-input"+(i*5+j)+"'class='form-control' value="+process[i].arrivalTime+"></input>");
+				input.appendTo(td);
+			}
+			if(j==2){
+				var input=$("<input id='edit-input"+(i*5+j)+"'class='form-control' value="+process[i].execTime+"></input>");
+				input.appendTo(td);
+			}
+			if(j==3){
+				var input=$("<input id='edit-input"+(i*5+j)+"'class='form-control' value="+process[i].period+"></input>");
+				input.appendTo(td);
+			}
+			if(j==4){
+			  var text=$('<a href="#" class="delete-process">&times</a>');
+			  text.appendTo(td);
+			}
+		}
+	}
 }
 
 // Function for showing and hiding Edit Case button
 function showEditCaseButton() {
 	if (typeof simulator === 'undefined') 
-		$('#edit-case').hide();
+		$('#edit-process').hide();
 	else
-		$('#edit-case').show(); 
+		$('#edit-process').show(); 
 }
 
 function resetObjList() {
@@ -193,7 +234,7 @@ function showCaseSettings() {
 	$("#scheme-text").html(simulator.scheme);
 
 	//CPU info show
-	var html="<table><tr>"
+	var html="<table class='table table-hover'><tr class='info'>"
 				+"<th>CID</th>"
 				+"<th>CPU name</th>"
 				+"<th>Algorithm</th></tr>";
@@ -209,12 +250,12 @@ function showCaseSettings() {
 	$("#resource-text").html(html);
 
 	//Process info show
-		html="<table><tr>"
-				+"<th>PID</th>"
-				+"<th>Process name</th>"
-				+"<th>Arrive Time</th>"
-				+"<th>Execution Time</th>"
-				+"<th>Period</th></tr>";
+	html="<table class='table table-hover'><tr class='info'>"
+			+"<th>PID</th>"
+			+"<th>Process name</th>"
+			+"<th>Arrive Time</th>"
+			+"<th>Execution Time</th>"
+			+"<th>Period</th></tr>";
 	for(var i=0;i<simulator.processList.length;i++){
 		var process = simulator.processList[i];
 		html+="<tr>"
@@ -229,4 +270,31 @@ function showCaseSettings() {
 	$("#process-text").html(html);
 }
 
+function showCaseInfo() {
+	$("#no-case-info-text").hide();
+	$("#case-info-div").show();
+}
 
+function resetCaseForm() {
+
+
+}
+
+function saveEditProcess() {
+	// // Create Process objects one by one and then add them to the processList
+	// for(var i=0;i<processQty;i++){
+	// 	var pid=process_manager.getNextID();
+	// 	var arrivalTime=$("#edit-input"+(i*4+1)).val();
+	// 	var execTime=$("#edit-input"+(i*4+2)).val();
+	// 	var period=$("#edit-input"+(i*4+3)).val();
+	// 	var process=new Process(pid,arrivalTime,execTime,period);
+	// 	process_manager.addProcess(process);
+	// 	// alert(process_manager.processList[i].arrivalTime+"//"+process_manager.processList[i].period);
+	// }
+	
+	// var rList = cpu_manager.CPUList;
+	// var pList = process_manager.processList;
+	// // Create a simulator object
+	// simulator = new Simulator(scheme, rList, pList);
+	
+}

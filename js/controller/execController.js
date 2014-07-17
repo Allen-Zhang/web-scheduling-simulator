1a1d60289fbf12cda6e1cbf6244c3d2255a06553
Allen Yingyuan Zhang <zyy2013@bu.edu>
2014-07-17 12:25:50 -0400

    Clean

--- js/controller/execController.js
+++
@@ -1,5 +0,0 @@
-
-var cpu_manager = new CPUManager();
-var process_manager = new ProcessManager();
-var recorder_manager = new RecorderManager();
-var simulator = new Simulator();  // Create an empty simulator object
